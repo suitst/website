d90762d2615f655096db24cedad5cd50a54ac903
suitst <suitst@tcd.ie>
2025-06-26 11:16:57 +0200

    added home page

--- timpage/svenska/views.py
+++ timpage/svenska/views.py
@@ -2,6 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('Hello world, you are at the svenska index.')
-
+    return HttpResponse("Hello world, you are at the svenska index.")
 
